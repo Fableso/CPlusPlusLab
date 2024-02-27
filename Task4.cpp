ef4ad4363c4935f9bb3fe202b4e156ab254046e1
Fableso <141618387+Fableso@users.noreply.github.com>
2024-02-27 15:33:47 +0200

    Update Task4.cpp

--- Task4.cpp
+++ Task4.cpp
@@ -1,25 +1,61 @@
 #include <iostream>
-#include <windows.h>
+#include <ctime>
+#include <cstdlib>
+#include <climits>
 
 using namespace std;
 
+void bubbleSort(int array[], int size) {
+    for (int i = 0; i < size - 1; ++i) {
+        for (int j = 0; j < size - i - 1; ++j) {
+            if (array[j] > array[j + 1]) {
+                swap(array[j], array[j+1]);
+            }
+        }
+    }
+}
+
+void fillArray(int arr[], int size)
+{
+    for (int i = 0; i < size; ++i) {
+        arr[i] = rand() - RAND_MAX / 2;
+    }
+}
+
+void outputArray(int arr[], int size)
+{
+    for (int i = 0; i < size; ++i) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+void combineArrays(int firstArray[], int secondArray[], int combinedArray[], int firstSize, int secondSize)
+{
+    for (int i = 0; i < firstSize; ++i) {
+        combinedArray[i] = firstArray[i];
+    }
+    for (int i = 0; i < secondSize; ++i) {
+        combinedArray[firstSize + i] = secondArray[i];
+    }
+}
+
 int main() {
-    SetConsoleOutputCP(CP_UTF8);
-    int n;
-    cout << "Введіть ціле число, яке не менше за 2: ";
-    cin >> n;
-
-    if (n < 2) {
-        cout << "Число повинне бути не менше 2." << endl;
-        return 0;
-    }
-
-    int i = 2;
-    while (n % i != 0) {
-        i++;
-    }
-
-    cout << "Найменший натуральний дільник, відмінний від 1, числa " << n << " є " << i << endl;
-
+    srand(static_cast<unsigned int>(time(0)));
+    int const n = 36;
+    int arr1[n], arr2[n];
+    fillArray(arr1, n);
+    fillArray(arr2, n);
+    cout << "Fist array: " << endl;
+    outputArray(arr1, n);
+    cout << "Second array: " << endl;
+    outputArray(arr2, n);
+    
+    int resultArr[n * 2];
+    combineArrays(arr1, arr2, resultArr, n, n);
+    bubbleSort(resultArr, 2 * n);
+    cout << endl << "Result arr: " << endl;
+    outputArray(resultArr, 2 * n);
+    
     return 0;
 }
